238fccbe2fa50854a2de23420eae24353bb68a94
Renekris <renekrispohlak@gmail.com>
2022-09-16 05:12:50 +0300

    Read entered data to object on submit

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 let myLibrary = [];
 
-function Book(name, startDate, endDate, hasRead = false) {
-    this.name = name;
-    this.startDate = startDate;
-    this.endDate = endDate;
-    this.hasRead = hasRead;
+const bookForm = document.getElementById('book-form');
+bookForm.addEventListener('submit', formSubmit)
+
+
+function Book(book) {
+    Object.assign(this, book);
 }
 
-function addBookToLibrary() {
-    let current = new Book('Harry Potter and the Philosopher\'s Stone', Date.now(), 'In the future', true);
-    myLibrary.push(current);
+function formSubmit(e) {
+    const book = {};
+
+    book.name = e.target[0]?.form[0]?.value;
+    book.startDate = e.target[1]?.form[1]?.value;
+    book.endDate = e.target[2]?.form[2]?.value;
+    book.hasRead = e.target[3]?.form[3]?.checked;
+
+    addBookToLibrary(book);
 }
 
-addBookToLibrary();
+function addBookToLibrary(book) {
+    myLibrary.push(new Book(book));
 
-console.log(myLibrary[0]);
+    console.log(myLibrary[myLibrary.length - 1])
+}
+
