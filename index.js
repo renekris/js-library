18eeb8ca83fc8e6144428f2651592f009494c01b
Renekris <renekrispohlak@gmail.com>
2022-09-19 03:24:50 +0300

    Change to UUID & get book values to setting modal

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ document.addEventListener('pointerup', function (e) {
     if (e.target == modal) modal.style.display = "none";
 })
 
+// constructor is made from form creation @function formSubmit
 function Book(book) {
     Object.assign(this, book);
 }
@@ -17,7 +18,7 @@ function Book(book) {
 function formSubmit(e) {
     const book = {};
 
-    book.id = getId();
+    book.id = crypto.randomUUID();
     book.name = e.target[0]?.form[0]?.value || '-';
     book.author = e.target[1]?.form[1]?.value || '-';
     book.pages = e.target[2]?.form[2]?.value || '-';
@@ -31,7 +32,7 @@ function getId() {
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(new Book(book));
+    myLibrary.push(new Book(book)); //
 
     addCard();
     console.log(myLibrary.length)
@@ -39,7 +40,7 @@ function addBookToLibrary(book) {
 }
 
 
-function updateBookDisplay() {
+function updateBookDisplay() { // for persistent storage
     for (let i = 0; i < myLibrary.length; i++) {
         const libraryDisplay = document.getElementById('library');
         const book = myLibrary[i];
@@ -91,14 +92,25 @@ function createCardElement(key) {
 function createCardSettings() {
     const settingsButton = document.createElement('button');
     settingsButton.classList.add('settings-button');
-    settingsButton.addEventListener('pointerup', settingsModal)
+    settingsButton.addEventListener('pointerup', displaySettingsModal)
 
     return settingsButton;
 }
 
-function settingsModal() {
+function displaySettingsModal(e) {
+    const currentLibraryIndex = myLibrary.filter(obj => obj.id === e.target.parentElement.attributes[0].value)[0];
     const modal = document.getElementById('modal');
+    const updateForm = document.getElementById('update-form');
+
     modal.style.display = 'flex';
+
+    console.dir("Settings UUID: " + e.target.parentElement.attributes[0].value);
+
+    updateForm[0].value = currentLibraryIndex.id;
+    updateForm[1].value = currentLibraryIndex.name;
+    updateForm[2].value = currentLibraryIndex.author;
+    updateForm[3].value = currentLibraryIndex.pages;
+    updateForm[4].checked = currentLibraryIndex.isFinished;
 }
 
 function createCardDelete() {
@@ -111,7 +123,7 @@ function createCardDelete() {
 
 function cardDelete(e) {
     const index = myLibrary.findIndex(obj => {
-        return obj.id === parseInt(e.target.parentElement.dataset.number);
+        return obj.id === e.target.parentElement.dataset.number;
     });
 
     if (index <= -1) return
