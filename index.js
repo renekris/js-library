1299ff3322a1f5e0245c184565005d869da8d0dd
Renekris <renekrispohlak@gmail.com>
2022-09-19 04:21:55 +0300

    Refactor & add update book values function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let myLibrary = [];
-let id = 0;
 
 const bookFormElement = document.getElementById('book-form');
-bookFormElement.addEventListener('submit', formSubmit)
+bookFormElement.addEventListener('submit', formSubmit);
+
+const updateFormElement = document.getElementById('update-form');
+updateFormElement.addEventListener('submit', updateSubmit)
 
 // close modal if target != modal content
 document.addEventListener('pointerup', function (e) {
@@ -10,35 +12,38 @@ document.addEventListener('pointerup', function (e) {
     if (e.target == modal) modal.style.display = "none";
 })
 
-// constructor is made from form creation @function formSubmit
+function updateSubmit(e) {
+    const currentIndex = myLibrary.findIndex(obj => obj.id === e.target[0].value);
+
+    myLibrary[currentIndex].id = e.target[0].value;
+    myLibrary[currentIndex].name = e.target[1].value;
+    myLibrary[currentIndex].author = e.target[2].value;
+    myLibrary[currentIndex].pages = e.target[3].value;
+    myLibrary[currentIndex].isFinished = e.target[4].checked;
+}
+
 function Book(book) {
     Object.assign(this, book);
 }
 
-function formSubmit(e) {
+function setNewLibraryValues(e) {
     const book = {};
 
     book.id = crypto.randomUUID();
-    book.name = e.target[0]?.form[0]?.value || '-';
-    book.author = e.target[1]?.form[1]?.value || '-';
-    book.pages = e.target[2]?.form[2]?.value || '-';
-    book.isFinished = e.target[3]?.form[3]?.checked;
+    book.name = e.target[0].value || '-';
+    book.author = e.target[1].value || '-';
+    book.pages = e.target[2].value || '-';
+    book.isFinished = e.target[3].checked; //default = false
 
-    addBookToLibrary(book);
+    return book;
 }
 
-function getId() {
-    return id++;
-}
+function formSubmit(e) {
+    const book = setNewLibraryValues(e);
 
-function addBookToLibrary(book) {
-    myLibrary.push(new Book(book)); //
-
+    myLibrary.push(new Book(book));
     addCard();
-    console.log(myLibrary.length)
-    console.log(myLibrary[myLibrary.length - 1])
 }
-
 
 function updateBookDisplay() { // for persistent storage
     for (let i = 0; i < myLibrary.length; i++) {
@@ -104,7 +109,7 @@ function displaySettingsModal(e) {
 
     modal.style.display = 'flex';
 
-    console.dir("Settings UUID: " + e.target.parentElement.attributes[0].value);
+    console.log(`%cCurrent UUID: ${e.target.parentElement.attributes[0].value}`, 'color: lightgreen');
 
     updateForm[0].value = currentLibraryIndex.id;
     updateForm[1].value = currentLibraryIndex.name;
@@ -122,12 +127,10 @@ function createCardDelete() {
 }
 
 function cardDelete(e) {
-    const index = myLibrary.findIndex(obj => {
-        return obj.id === e.target.parentElement.dataset.number;
-    });
+    const currentIndex = myLibrary.findIndex(obj => obj.id === e.target.parentElement.dataset.number);
 
-    if (index <= -1) return
-    myLibrary.splice(index, 1);
+    if (currentIndex <= -1) return
+    myLibrary.splice(currentIndex, 1);
     e.target.parentElement.remove();
 
     console.log(myLibrary);
